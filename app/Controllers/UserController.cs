878fb502beb96219eb250494aa677127a0715a7a
Kinga Anna Lipiszko <kin.li@wp.pl>
2020-11-10 22:13:16 +0100

    FREECB2020-57 #time 30m stworzenie widoku rejestracji

--- app/Controllers/UserController.cs
+++ app/Controllers/UserController.cs
@@ -19,6 +19,11 @@ namespace app.Controllers
             return View();
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
