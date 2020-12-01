d8edf5d74f19eb4d5a144bf94cff65f38f3e9f1c
PawełNiewęgłowski <pawelnwg@gmail.com>
2020-12-01 18:07:58 +0100

    #FREECB2020-67 #time 4h Aktualizacja rejestracji, poprawa widoków, modeli, kontrolerow, dodanie logiki, reqex, walidacja

--- app/Models/Credentials.cs
+++ app/Models/Credentials.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace freelancerzy.Models
 {
     public partial class Credentials
     {
         public int Userid { get; set; }
+
+        [Required(ErrorMessage ="Hasło jest wymagane")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$", ErrorMessage = "Hasło musi posiadać min 8 znaków, wielkie litery, małe oraz znaki specjalne")]
+        [Display(Name = "Hasło")]
         public string Password { get; set; }
+
+
+        [NotMapped]
+        [Required(ErrorMessage = "Potwierdzenie hasła jest wymagane")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Hasła nie są identyczne")]
+        [Display(Name = "Powtórz hasło")]
+        public string PasswordConfirmed { get; set; }
 
         public virtual PageUser User { get; set; }
     }
--- app/Models/PageUser.cs
+++ app/Models/PageUser.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace freelancerzy.Models
 {
     public partial class PageUser
     {
         public PageUser()
-        {
+        {           
             MessageUserFrom = new HashSet<Message>();
             MessageUserTo = new HashSet<Message>();
             Offer = new HashSet<Offer>();
@@ -15,10 +16,25 @@ namespace freelancerzy.Models
 
         public int Userid { get; set; }
         public int TypeId { get; set; }
-      
+
+        [Required(ErrorMessage = "Imie jest wymagane")]
+        [MaxLength(20, ErrorMessage = "Imie nie może być dłuższe niż 20 znaków")]
+        [Display(Name = "Imię")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Nazwisko jest wymagane")]
+        [MaxLength(30, ErrorMessage = "Nazwisko nie może być dłuższe niż 30 znaków")]
+        [Display(Name = "Nazwisko")]
         public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Email jest wymagany")]
+        [MaxLength(40, ErrorMessage = "Email nie może być dłuższy niż 40 znaków")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Niepoprawny adres")]
+        [Display(Name = "Email")]
         public string EmailAddress { get; set; }
+
+        [Display(Name = "Numer telefonu")]
+        [RegularExpression(@"^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$", ErrorMessage = "Niepoprawny numer")]
         public int? Phonenumber { get; set; }
         public bool emailConfirmation { get; set; }
 
