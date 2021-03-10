07b23a138f5f050a4d865e8378ff3f4a80e019da
Michal <michal240499@gmail.com>
2021-03-10 11:47:23 +0100

    #FREECB2020-92 #time 4h wyslanie wygenerowanego hasla na maila

---
+++ app/Models/MailPasswd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace freelancerzy.Models
+{
+    public class MailPasswd
+    {
+        private String smtpEmailAdress = "freeelancerzy@gmail.com";
+        public String SmtpEmailAdress { get { return smtpEmailAdress; } }
+
+        private String emailSubject = "Odzyskanie hasła";
+        public String EmailSubject { get { return emailSubject; } }
+
+        private String smtpHost = "smtp.gmail.com";
+        public String SmtpHost { get { return smtpHost; } }
+
+        private int smtpPort = 587;
+        public int SmtpPort { get { return smtpPort; } }
+
+        public String EmailBody { get { return emailBody; } }
+        private String emailBody = File.ReadAllText("Data/mailBodyPsswd.html");
+    }
+}
