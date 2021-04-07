63a6b8e3ee9d0c9a9fecabb8205f7cdc89a48e93
Marcin Orzechowski <marcinorz@o2.pl>
2021-04-07 14:08:16 +0200

    FREECB2020-39 #time 4h widok chatu, wysylanie wiadomosci, proba zrobienia wysylania asnchronicznego

---
+++ app/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using freelancerzy.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace freelancerzy.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
